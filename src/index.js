8fb7632e70132f38593665a82e29884f16628c77
Jvictor-7 <jvcl@ic.ufal.br>
2023-11-29 23:16:56 -0300

    structured folder, and challenge finished!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import sum from './sum.js'
 import readInput from './input.js'
 
+// Welcome print
+console.log("Technical Challenge One from enterprise: Escribo Inovação para o Aprendizado\n")
+
 // Reading the number 
 let number = readInput()
 
